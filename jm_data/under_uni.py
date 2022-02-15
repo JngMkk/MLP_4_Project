0b97459822c13f46c8dbea1a65021ea0b8bca40b
JoongMo Kang <87686562+JngMkk@users.noreply.github.com>
2022-02-15 23:32:18 +0900

    refine datas -joongmo

--- jm_data/under_uni.py
+++ jm_data/under_uni.py
@@ -1,8 +1,8 @@
-# 정부재정지원제한대학(부실대학) 기준인 충원율 70퍼 미만 대학
+# 정부재정지원제한대학(부실대학) 기준인 신입생 충원율 97% 미만 대학
 import pandas as pd
 
-datas = ['uni_2019', 'uni_2020', 'uni_2021']
+datas = ['uni_freshman_2019', 'uni_freshman_2020', 'uni_freshman_2021']
 
 for data in datas:
     df = pd.read_csv(f'./data/{data}.csv', index_col=0)
-    df[df.iloc[:, [11]].values < 70].to_csv(f'./data/under_{data}')
+    df[df.iloc[:, [11]].values < 97].to_csv(f'./data/under_{data}')
