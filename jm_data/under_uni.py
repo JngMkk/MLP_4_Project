2e25bf7b8f24f9248ce65dac663bf40daaf75064
JoongMo Kang <87686562+JngMkk@users.noreply.github.com>
2022-02-16 11:22:45 +0900

    update

--- jm_data/under_uni.py
+++ jm_data/under_uni.py
@@ -6,12 +6,12 @@ datas = ['uni_freshman_2019', 'uni_freshman_2020', 'uni_freshman_2021']
 for data in datas:
     df = pd.read_csv(f'./data/{data}.csv', index_col=0)
     df = df[df.iloc[:, [11]].values < 97]
-    df = df.reset_index()
+    df = df.reset_index(drop=True)
     df.to_csv(f'./data/under_{data}.csv')
 
 datas = ['uni_students_2019', 'uni_students_2020', 'uni_students_2021']
 for data in datas:
     df = pd.read_csv(f'./data/{data}.csv', index_col=0)
     df = df[df.iloc[:, [11]].values < 86]
-    df = df.reset_index()
+    df = df.reset_index(drop=True)
     df.to_csv(f'./data/under_{data}.csv')
