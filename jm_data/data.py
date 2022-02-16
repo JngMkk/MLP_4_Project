35942efcac96d2ee2fd61d6f8d458970b54fc8c4
JoongMo Kang <87686562+JngMkk@users.noreply.github.com>
2022-02-16 11:20:00 +0900

    update

--- jm_data/data.py
+++ jm_data/data.py
@@ -9,7 +9,7 @@ def file_sort(file):
     df.to_csv(f'./data/{file}.csv')
 
 def file_sort2(file):
-    df = pd.read_excel(f'./resources/uni_students_2019.xlsx', header=4)
+    df = pd.read_excel(f'./resources/{file}.xlsx', header=4)
     df = df.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11,12]]
     df.columns = ['기준연도', '학교종류', '설립구분', '지역', '상태', '학교', '학생정원', '학생모집정지인원', '총재학생', '정원내재학생', '정원외재학생', '재학생충원율[총재학생/(학생정원-정지인원)x100]', '정원내학생충원율[정원내재학생/(학생정원-정지인원)x100]']
     df.to_csv(f'./data/{file}.csv')
