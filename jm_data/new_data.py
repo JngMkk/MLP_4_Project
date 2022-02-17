f2ac06bfd7f87220f42eaf31392cf4ac96199385
seha3349 <sangdong.lee33@gmail.com>
2022-02-17 19:07:12 +0900

    SD - modified new data

---
+++ jm_data/new_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+df_coll_2019 = pd.read_excel('./data_new/college_recruit_2019.xlsx')
+df_univ_2019 = pd.read_excel('./data_new/univ_recruit_2019.xlsx')
+
+print(df_coll_2019)
+print(df_univ_2019)
